e7f9bb4fe67a6f8ddc513e5d935859b60efc15d4
Hunobas <po127992@gmail.com>
2021-10-12 18:01:23 +0900

    ~7. Antialiasing

--- RayTracingOneWeek/3.TheVec3Class/4.RaysSimpleCameraBackground.cpp
+++ RayTracingOneWeek/3.TheVec3Class/4.RaysSimpleCameraBackground.cpp
@@ -2,8 +2,8 @@
 
 #include "hittable_list.h"
 #include "color.h"
-#include "ppm.h"
 #include "sphere.h"
+#include "camera.h"
 
 #include <iostream>
 
@@ -13,7 +13,7 @@ color ray_color(const ray& r, const hittable& world)
 
 	if (world.hit(r, 0, infinity, rec))
 	{
-		return 0.5 * (rec.normal + color(1.0, 1.0, 1.0));
+		return 0.5 * (rec.normal + color(1, 1, 1));
 	}
 
 	vec3 unit_direction = unit_vector(r.direction());
@@ -29,6 +29,7 @@ int main()
 	const auto aspect_ratio = 16.0 / 9.0;
 	const int image_width = 400;
 	const int image_height = static_cast<int>(image_width / aspect_ratio);
+	const int samples_per_pixel = 200;
 
 	PPM ppm1(image_height, image_width);
 
@@ -40,38 +41,31 @@ int main()
 
 	// camera
 
-	auto viewport_height = 2.0;
-	auto viewport_width = aspect_ratio * viewport_height;
-	auto focal_length = 1.0;
-
-	auto origin = point3(0, 0, 0);
-	auto horizontal = vec3(viewport_width, 0, 0);
-	auto vertical = vec3(0, viewport_height, 0);
-	auto lower_left_corner = origin - horizontal / 2 - vertical / 2 - vec3(0, 0, focal_length);
+	camera cam;
 
 	// render
-
-	std::cout << "p3\n" << image_width << " " << image_height << "\n255\n";
 
 	for (int j = image_height - 1; j >= 0; --j)
 	{
 		std::cerr << "\rscanlines remaining: " << j << ' ' << std::flush;
 		for (int i = 0; i < image_width; ++i)
 		{
-			auto u = double(i) / (image_width - 1);
-			auto v = double(j) / (image_height - 1);
-			ray r(origin, lower_left_corner + u * horizontal + v * vertical - origin);
-			color pixel_color = ray_color(r, world);
-			//write_color(std::cout, pixel_color);
+			color pixel_color(0, 0, 0);
 
-			ppm1.image[j][i].r = static_cast<int>(255.999 * pixel_color.x());
-			ppm1.image[j][i].g = static_cast<int>(255.999 * pixel_color.y());
-			ppm1.image[j][i].b = static_cast<int>(255.999 * pixel_color.z());
+			for (int s = 0; s <= samples_per_pixel; ++s)
+			{
+				auto u = double(i) / (image_width - 1);
+				auto v = double(j) / (image_height - 1);
+				ray r = cam.get_ray(u, v);
+				pixel_color += ray_color(r, world);
+			}
+
+			write_color(ppm1, j, i, pixel_color, samples_per_pixel);
 		}
 	}
 
 	ppm1.set_version("P3");
-	ppm1.save("Result.ppm"); 
+	ppm1.save("Result.ppm");
 
 	std::cerr << "\nDone.\n";
 
